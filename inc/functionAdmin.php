7b4305ace93aad3ea622911d8ed31a4b1444f0d4
Wildhound <wildhound404@gmail.com>
2022-04-21 19:15:02 +0600

    vid no4

--- inc/functionAdmin.php
+++ inc/functionAdmin.php
@@ -3,9 +3,18 @@
 add_action( "admin_menu", "novemberAdminPage");
 
 function novemberAdminPage(){
+    //generate admin main page & menu
     add_menu_page( "Noverber Theme Options", "November", "manage_options", "novemberNight", "novemberAdminPageGen","dashicons-admin-customizer", 110);
+
+    // generate admin sub page
+    add_submenu_page( "novemberNight", "Noverber Theme Options", "Settings", "manage_options", "novemberNight", "novemberAdminPageGen");
+    add_submenu_page( "novemberNight", "Noverber Custom Css", "Custom CSS", "manage_options", "novemberNightCss", "noverberCustomCss");
 }
 
 function novemberAdminPageGen(){
-    echo "<h1>Hello Admin Page</h1>";
+    require_once(get_template_directory()."/inc/templates/admin.php");
+}
+
+function noverberCustomCss(){
+    echo "<h1>Hello Custom Css Page</h1>";
 }
---
+++ inc/templates/admin.php
@@ -0,0 +1,3 @@
+<h1>Hello Admin Page</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize theme options here</p>
